c8fa44a482398237fbd8e1cab9b166a1e51448b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-27 22:22:04 +0000

    Deploy jh-code/ng-cms to github.com/jh-code/ng-cms.git:gh-pages

--- src/app/app.constants.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_ROLES = ['reader', 'author', 'admin'];
-export const DEFAULT_THEMES = ['dark-theme', 'light-theme'];
--- src/app/models/setting.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Setting {
-  name: string;
-  value: any;
-  canDelete: boolean;
-  canEdit: boolean;
-  show: boolean;
-  type: string;
-  options?: any[] | string;
-}
