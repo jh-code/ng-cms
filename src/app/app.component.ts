34df0e1032095fb0583c7ecb87e7763a80bcc416
jh-code <jh.wphax@gmail.com>
2018-10-04 01:35:18 -0500

    feat: theme switcher in header menu
    service added to switch themes, soon will save selection to localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,41 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, OnDestroy } from '@angular/core';
+import { ThemeService } from './services/theme/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  private theme = 'dark-theme';
+export class AppComponent implements OnInit, OnDestroy {
+  private themeSub: Subscription;
+  private currentTheme: string;
 
   constructor(
     private el: ElementRef,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private themeService: ThemeService
   ) {
     //
   }
 
   public ngOnInit(): void {
-    //
+    this.themeSub = this.themeService.theme.subscribe((theme: string): void => {
+      console.log(theme);
+      this.setTheme(theme);
+      this.currentTheme = theme;
+    });
   }
 
   public setTheme(className: string): void {
-    if (this.theme !== '') {
-      this.renderer.removeClass(this.el.nativeElement, this.theme);
-    }
-
+    console.log(className, this.currentTheme);
     if (className !== '') {
+      this.renderer.removeClass(this.el.nativeElement, this.currentTheme);
       this.renderer.addClass(this.el.nativeElement, className);
     }
+  }
 
-    this.theme = className;
+  public ngOnDestroy(): void {
+    this.themeSub.unsubscribe();
   }
 }
---
+++ src/app/services/theme/theme.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ThemeService } from './theme.service';
+
+@NgModule({
+  providers: [ThemeService]
+})
+export class ThemeModule {
+  //
+}
